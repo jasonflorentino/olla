066fc40452336dbfe1077933ecf88b34656d54d3
jason <72258481+jasonflorentino@users.noreply.github.com>
2025-08-27 23:47:49 -0400

    wip generate chat completion

--- src/lib/chat-context.tsx
+++ src/lib/chat-context.tsx
@@ -1,13 +1,18 @@
 import React, { createContext, useContext, useMemo, useState } from "react";
+import type { Message } from "./api";
 
 type ChatContextState = {
   message: string;
   setMessage: (message: string) => void;
+  messages: Message[];
+  setMessages: (messages: Message[]) => void;
 };
 
 const initialState: ChatContextState = {
   message: "",
   setMessage: () => null,
+  messages: [],
+  setMessages: () => null,
 };
 
 const ChatContext = createContext<ChatContextState>(initialState);
@@ -15,15 +20,18 @@ const ChatContext = createContext<ChatContextState>(initialState);
 export const useChatContext = () => useContext(ChatContext);
 
 export const ChatProvider = ({ children }: { children: React.ReactNode }) => {
-  const [message, setMessage] = useState("")
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState<Message[]>([]);
 
-  const value = useMemo(() => ({
-    message,setMessage
-  }), [message, setMessage])
+  const value = useMemo(
+    () => ({
+      message,
+      setMessage,
+      messages,
+      setMessages,
+    }),
+    [message, setMessage, messages, setMessages],
+  );
 
-  return (
-    <ChatContext.Provider value={value}>
-      { children }
-    </ChatContext.Provider>
-  )
-}
+  return <ChatContext.Provider value={value}>{children}</ChatContext.Provider>;
+};
