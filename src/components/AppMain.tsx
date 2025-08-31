29c7eb0f79100848e2323e3470ad78c437fc4f6c
jason <72258481+jasonflorentino@users.noreply.github.com>
2025-08-31 15:54:25 -0400

    little more stying

--- src/components/AppMain.tsx
+++ src/components/AppMain.tsx
@@ -13,13 +13,13 @@ export function AppMain() {
   const isSettings = page === Page.Settings;
 
   return (
-    <main className="px-6 py-4">
+    <main className="px-4 sm:px-5 md:px-6 py-4">
       {isHome && (
         <>
           <ChatHistory />
           <div className="lg:max-w-[800px] lg:mx-auto">
             <ChatInput>
-              <div className="flex gap-3 justify-end absolute bottom-4 right-4">
+              <div className="flex gap-3 justify-end absolute bottom-3 right-3 sm:bottom-4 sm:right-4">
                 <ChatClear />
                 <ChatSubmit />
               </div>
