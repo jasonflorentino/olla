1775c770af98d53dae2d07be7019d972b3910f89
jason <72258481+jasonflorentino@users.noreply.github.com>
2025-08-31 15:43:49 -0400

    header styles

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -11,10 +11,10 @@ export function AppHeader() {
 
   return (
     <header className="flex flex-col md:flex-row justify-between items-center px-6 py-4">
-      <h1 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+      <h1 className="scroll-m-20 md:border-b pb-1 text-3xl font-semibold tracking-tight first:mt-0">
         🐱Olla
       </h1>
-      <div className="flex gap-3">
+      <div className="flex gap-1 sm:gap-2 md:gap-3 mt-2">
         {isHome && <ModelThinkSelect />}
         {isHome && <ModelSelect />}
         <ThemeToggle />
