3623ab05bcd98f87ea8281046a5781e56ed170eb
Jason Florentino <72258481+jasonflorentino@users.noreply.github.com>
2026-04-15 23:52:33 -0400

    Updates (#6)
    * prompts
    
    * add badge
    
    * add current settings header

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -1,12 +1,31 @@
 import { ThemeToggle } from "./ThemeToggle";
 import { SettingsButton } from "./SettingsButton";
+import { Badge } from "@/components/ui";
+import { useModelContext } from "@/lib/context";
+import { CircleUser, Bot } from "lucide-react";
 
 export function AppHeader() {
+  const { prompt, prompts, model } = useModelContext();
+
+  const promptName =
+    Object.entries(prompts).find(([, p]) => p === prompt)?.[0] ?? "(none)";
+  const modelName = model.split(":")?.[0] ?? "(none)";
+
   return (
     <header className="flex flex-col md:flex-row justify-between items-center px-6 py-4">
       <h1 className="scroll-m-20 md:border-b pb-1 text-3xl font-semibold tracking-tight first:mt-0">
         🐱Olla
       </h1>
+      <div className="flex gap-1 sm:gap-2 md:gap-3 mt-2">
+        <Badge variant="secondary">
+          <CircleUser />
+          {promptName}
+        </Badge>
+        <Badge variant="outline">
+          <Bot />
+          {modelName}
+        </Badge>
+      </div>
       <div className="flex gap-1 sm:gap-2 md:gap-3 mt-2">
         <ThemeToggle />
         <SettingsButton />
--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -1,3 +1,4 @@
 export * as Text from "./text";
 export { Switch } from "./switch";
+export { Badge } from "./badge";
 export { Button } from "./button";
